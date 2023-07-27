f1b0c76605a78236a746386eca6a1b6954bb7efa
nelen11 <neleen11@gmail.com>
2023-07-27 20:05:53 +0545

    newsletter app

---
+++ app.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require('https');
+require('dotenv').config();
+
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/signup.html"); 
+});
+
+app.post("/", function(req, res){
+
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.email;
+
+    const data= {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    };
+
+    const jsonData = JSON.stringify(data);
+
+    const url = `https://us9.api.mailchimp.com/3.0/lists/c6c2ac8c7b`;
+
+    const api_key = process.env.API_KEY;
+
+    const options = {
+        method: "POST",
+        auth: `nelen1:${api_key}`
+    }
+
+    const request = https.request(url, options, function(response){
+
+        if (response.statusCode === 200){
+            res.sendFile(__dirname + "/success.html");
+        }else{
+            res.sendFile(__dirname + "/failure.html");
+        }
+
+
+        response.on("data", function(data){
+            console.log(JSON.parse(data));
+        })
+    })
+
+    request.write(jsonData);
+    request.end();
+    
+
+});
+
+app.post("/failure", function(req, res){
+    res.redirect("/")
+})
+
+
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Server is running on port 3000");
+});
+
+//Api key
+// f01dc7f2ad3bd0f8ac539cd850baf852-us9
+
+// List Id
+// c6c2ac8c7b
